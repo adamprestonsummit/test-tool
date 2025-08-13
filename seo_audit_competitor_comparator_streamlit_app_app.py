b6c511cc5f8a8fee611ac323099721922a3b011a
adamprestonsummit <adam.preston@summitmedia.com>
2025-08-13 15:33:51 +0100

    Update seo_audit_competitor_comparator_streamlit_app_app.py

--- seo_audit_competitor_comparator_streamlit_app_app.py
+++ seo_audit_competitor_comparator_streamlit_app_app.py
@@ -1437,8 +1437,8 @@ if run_btn and default_domain:
 
             # RECOMMENDATIONS
             with st.container(border=True):
-                st.markdown("<div class='section-title'>Recommendations</div>", unsafe_allow_html=True)
-                    recs = res.get("_recommendations", []) or []
+            st.markdown("<div class='section-title'>Recommendations</div>", unsafe_allow_html=True)
+            recs = res.get("_recommendations", []) or []
                 if recs:
                     for m in recs:
                         st.write("•", m)
